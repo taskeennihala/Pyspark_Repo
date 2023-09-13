852705bf4242399f0786f93d674d6bb590ffb04f
taskeennihala <141902334+taskeennihala@users.noreply.github.com>
2023-09-13 22:04:46 +0530

    Update driver.py

--- src/Assignment1/driver.py
+++ src/Assignment1/driver.py
@@ -23,37 +23,44 @@ schema = StructType([
          StructField('gender', StringType(), True),
          StructField('salary', IntegerType(), True)
          ])
+
 #createing a data frame
 df = dataframe(spark,dataDF,schema)
 df.show()
+
 #Select firstname, lastname and salary from Dataframe.
 selected = selecting_col(df,df.name.firstname,df.name.lastname,df.salary)
 #selected.show()
+
 #Add Country, department, and age column in the dataframe.
 df = creating_col(df,'country','USA')
 df = creating_col(df,'department','ECE')
 df = creating_col(df,'age',22)
 #df.show()
+
 #Change the value of salary column.
 df=add_salary(df,'salary','100')
 #df.show()
+
 #Change the data types of DOB and salary to String
 df = changing_data_type(df,'dob','string')
 df = changing_data_type(df,'salary','string')
 #df.show()
+
 #Derive new column from salary column.
 df = new_column(df,'newsalary','salary','2000')
 #df.show()
-
 
 #Filter the name column whose salary in maximum.
 df = changing_data_type(df,'salary','integer')
 df = max_salary(df,'salary')
 #df.show()
+
 #Drop the department and age column.
 df = droping_column(df,'department')
 df = droping_column(df,'age')
 #df.show()
+
 #List out distinct value of dob and salary
 df1=duplicate(df,column=('dob'))
 df2=duplicate(df,column=('salary'))
