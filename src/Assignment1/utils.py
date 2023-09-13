83913357514db9a8791d3c340b03fd187402c326
taskeennihala <141902334+taskeennihala@users.noreply.github.com>
2023-09-13 22:05:20 +0530

    Update utils.py

--- src/Assignment1/utils.py
+++ src/Assignment1/utils.py
@@ -2,7 +2,6 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col,max
 
-
 def create_spark():
     spark =  SparkSession.builder.appName("Pyspark_Assignment1").getOrCreate()
     return spark
@@ -20,19 +19,16 @@ def add_salary(df,salary,value):
     return adding_salary
 def changing_data_type(df,column,type):
     change_type = df.withColumn(column,col(column).cast(type))
-
     return change_type
 def new_column(df,newsalary,salary,value):
     adding_sal_col = df.withColumn('newsalary',col(salary)+value)
     return adding_sal_col
-
 def max_salary(df,salary):
     filtering_col = df.select(max(salary))
     return filtering_col
 def droping_column(df,column):
     drop_column = df.drop(column)
     return drop_column
-
 def duplicate(df,column):
     df1=df.select(column).distinct()
     return df1
