6b39179d26695913eb6c8f0c6fe7a935c6e684ab
taskeennihala <141902334+taskeennihala@users.noreply.github.com>
2023-09-16 21:16:15 +0530

    Update driver.py

--- src/Assignment3/driver.py
+++ src/Assignment3/driver.py
@@ -1,39 +1,37 @@
-from pyspark.sql.types import StructType, StructField, IntegerType
+import pyspark
+from util import *
+from pyspark.sql import *
+from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
-from Pyspark_Repo.src.Assignment1.driver import spark
-from Pyspark_Repo.src.Assignment3.utils import row_data, dept_grp, highest_salary, multi_action
+spark=create_SparkSession()
 
-employee_schema = StructType([StructField("employee_name", StringType(), True),
-                              StructField("department", StringType(), True),
-                              StructField("salary", IntegerType(), True)
-                              ])
-employee_data = [("James", "Sales", 3000),
-                 ("Michael", "Sales", 4600),
-                 ("Robert", "Sales", 4100),
-                 ("Maria", "Finance", 3000),
-                 ("Raman", "Finance", 3000),
-                 ("Scott", "Finance", 3300),
-                 ("Jen", "Finance", 3900),
-                 ("Jeff", "Marketing", 3000),
-                 ("Kumar", "Marketing", 2000)]
+schema1 = StructType([
+    StructField('employee_name',StringType(),True),
+    StructField('department',StringType(),True),
+    StructField('salary',IntegerType(),True)
+])
 
+data1 = [
+    ('James','Sales',3000),
+    ('Michael','Sales',4600),
+    ('Robert','Sales',4100),
+    ('Maria','Finance',3000),
+    ('Raman','Finance',3000),
+    ('Scott','Finance',3300),
+    ('Jen','Finance',3900),
+    ('Jeff','Marketing',3000),
+    ('Kumar','Marketing',2000)
+]
+df=create_dataframe(spark,data1,schema1)
 
-employee_df = crete_df(spark,employee_data,employee_schema)
-employee_df.show()
+# Select first row from each department group.
+df1=first_row(df).show()
 
-#2.	Select first row from each department group
-dept_df = dept_grp(employee_df)
-dept_df.show()
+# Retrieve Employees who earns the highest salary.
+df2=highest_salary(df)
+df2.show()
 
-#3.	Create a Dataframe from Row and List of tuples.
-new_df = row_data(spark)
-new_df.show()
-
-#5.	Retrieve Employees who earns the highest salary
-highest_sal = highest_salary(employee_df)
-highest_sal.show()
-
-#6.	Select the highest, lowest, average, and total salary for each department group.
-multi_df = multi_action(employee_df)
-multi_df.show()
+# Select the highest, lowest, average, and total salary for each department group.
+df3=totalsal_avg_high_low(df)
+df3.show()
